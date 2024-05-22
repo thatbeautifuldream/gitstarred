47071e0a274ba57bfea3b7dbef43b6c0d6e369f0
Milind Mishra <milind.mishra4@gmail.com>
2024-05-22 05:52:21 +0530

    🐛 FIX: remove unwanted packages

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,14 +1,11 @@
 import { ViewTransitions } from "next-view-transitions";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ViewTransitions>
-      <ClerkProvider>
-        {children}
-        <Toaster position="top-center" richColors />
-      </ClerkProvider>
+      {children}
+      <Toaster position="top-center" richColors />
     </ViewTransitions>
   );
 }
