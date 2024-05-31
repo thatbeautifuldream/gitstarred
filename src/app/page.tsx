11214c05e1c749d62034a2ce46b2a48defe5aba7
Milind Mishra <milind.mishra4@gmail.com>
2024-05-31 08:42:32 +0530

    🐛 FIX: update example env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -18,7 +17,7 @@ export default function Page() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Get starrrrrrted! 😉
+            Git starrrrrred 🌟
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
